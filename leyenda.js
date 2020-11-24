bad712ddd8b063ff6c4c9134ec8a1417f13629a0
Marco Loli <67282645+marcoAlexander21@users.noreply.github.com>
2020-11-23 23:23:06 -0500

    Hide two files

--- leyenda.js
+++
@@ -1,8 +0,0 @@
-var l = document.getElementById("leyeCan");
-var lc = l.getContext("2d");
-var imgl = new Image();
-imgl.src = "imagenes/serpiente.png";
-imgl.onload = function()
-{
-    lc.drawImage(imgl, 50, 0)
-}
